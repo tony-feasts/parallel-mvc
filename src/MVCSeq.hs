788479f95d3d35624b68007bfd5edb10b9bfc5a0
tony-feasts <aug2102@columbia.edu>
2024-12-04 17:59:55 -0500

    Fix bfs

--- src/MVCSeq.hs
+++ src/MVCSeq.hs
@@ -3,7 +3,6 @@ module MVCSeq (solve) where
 import qualified Data.Set as S
 import qualified Data.Sequence as Seq -- document Seq
 import qualified Data.Vector as V
-import Data.Sequence (Seq(..))
 
 -- | Solve the Minimum Vertex Cover problem using BFS
 solve :: Int -> Int -> V.Vector [Int] -> [Int]
@@ -13,22 +12,22 @@ solve n m adjList = bfs (Seq.singleton ([], S.empty, 1))
               | S.size cover == m = subset
               | otherwise =
                 bfs (enqueueNextSubsets (subsets, idx) subset cover)
-  
-          -- Add all possible next states to the queue
+
+          -- Add all possible ways to extend a subset to the queue
           enqueueNextSubsets (subsets, idx) subset cover
               | idx > n = subsets
               | otherwise =
                 enqueueNextSubsets
                     (processVertex subsets idx subset cover) subset cover
-  
-          -- Add the next state for a subset to the queue
+
+          -- Add a subset extension to the queue
           processVertex subsets v subset cover =
               let newSubset = v : subset
                   newCover = insertEdges cover (adjList V.! v)
                   insertEdges edges [] = edges
                   insertEdges edges (u:us) =
                     insertEdges (S.insert (min v u, max v u) edges) us
-  
+
               in if S.size newCover == m
-                 then ((Seq.singleton (newSubset, S.empty, 0)), (n + 1))
+                 then ((Seq.singleton (newSubset, newCover, v + 1)), (n + 1))
                  else ((subsets Seq.|> (newSubset, newCover, v + 1)), (v + 1))
